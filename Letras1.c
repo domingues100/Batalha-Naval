fbc1f3725a890992960b8090c245518daf17166b
Gabriel Montagni Domingues Filho <62193410+domingues100@users.noreply.github.com>
2020-06-09 11:37:16 -0300

    Create Letras1.c

---
+++ Letras1.c
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+
+int main (){
+//line 1
+printf("%c%c%c%c%c%c",219, 219, 219, 219, 219, 219);
+printf("      %c%c%c%c  ",219, 219,219, 219);
+printf("  %c%c%c%c%c%c%c%c",219, 219, 219, 219, 219, 219, 219, 219);
+printf("    %c%c%c%c  ",219, 219,219, 219);
+printf("  %c%c   ",219 ,219);
+printf("     %c%c    %c%c  ",219 ,219, 219,219);
+printf("  %c%c%c%c  ",219, 219,219, 219);
+printf("\n");
+//line 2
+printf("%c%c    %c%c",219 , 219), 219, 219;
+printf("  %c%c    %c%c",219 , 219);
+printf("     %c%c   ",219 , 219);
+printf("  %c%c    %c%c",219 , 219);
+printf("  %c%c   ",219 ,219);
+printf("     %c%c    %c%c  ",219 ,219, 219,219);
+printf("%c%c    %c%c",219 , 219);
+printf("\n");
+//line 3
+printf("%c%c%c%c%c%c", 219, 219, 219, 219, 219, 219);
+printf("    %c%c    %c%c", 219, 219, 219, 219);
+printf("     %c%c   ", 219, 219);
+printf("  %c%c    %c%c", 219, 219, 219, 219);
+printf("  %c%c   ", 219,219);
+printf("     %c%c%c%c%c%c%c%c",219, 219, 219, 219, 219, 219, 219, 219);
+printf("  %c%c    %c%c", 219, 219, 219, 219);
+printf("\n");
+//line 4
+printf("%c%c    %c%c", 219, 219, 219, 219);
+printf("  %c%c%c%c%c%c%c%c",219, 219, 219, 219, 219, 219, 219, 219);
+printf("     %c%c   ",219, 219);
+printf("  %c%c%c%c%c%c%c%c",219, 219, 219, 219, 219, 219, 219, 219);
+printf("  %c%c   ",219,219);
+printf("     %c%c    %c%c  ",219 ,219, 219,219);
+printf("%c%c%c%c%c%c%c%c",219, 219, 219, 219, 219, 219, 219, 219);
+printf("\n");
+//line 5
+printf("%c%c%c%c%c%c",219, 219, 219, 219, 219, 219);
+printf("    %c%c    %c%c", 219, 219, 219, 219);
+printf("     %c%c   ", 219, 219);
+printf("  %c%c    %c%c", 219, 219, 219, 219);
+printf("  %c%c%c%c%c%c%c%c",219, 219, 219, 219, 219, 219, 219, 219);
+printf("  %c%c    %c%c",219 ,219, 219,219);
+printf("  %c%c    %c%c", 219, 219, 219, 219);
+printf("\n\n");
+//line 6
+printf("         %c%c    %c%c",219 ,219, 219,219);
+printf("    %c%c%c%c  ",219, 219,219, 219);
+printf("  %c%c      %c%c",219 ,219, 219,219);
+printf("    %c%c%c%c  ",219, 219,219, 219);
+printf("  %c%c     ",219 ,219);
+printf("\n");
+//line 7
+printf("         %c%c%c%c  %c%c",219 ,219, 219,219, 219, 219);
+printf("  %c%c    %c%c",219 , 219);
+printf("  %c%c      %c%c",219 ,219, 219,219);
+printf("  %c%c    %c%c",219 , 219);
+printf("  %c%c   ",219 ,219);
+printf("\n");
+//line 8
+printf("         %c%c  %c%c%c%c",219, 219, 219, 219, 219, 219, 219, 219);
+printf("  %c%c    %c%c",219 , 219);
+printf("  %c%c      %c%c",219 ,219, 219,219);
+printf("  %c%c    %c%c",219 , 219);
+printf("  %c%c   ",219 ,219);
+printf("\n");
+//line 9
+printf("         %c%c    %c%c",219 ,219, 219,219);
+printf("  %c%c%c%c%c%c%c%c",219, 219, 219, 219, 219, 219, 219, 219);
+printf("    %c%c  %c%c   ",219 ,219, 219,219);
+printf(" %c%c%c%c%c%c%c%c",219, 219, 219, 219, 219, 219, 219, 219);
+printf("  %c%c     ",219 ,219);
+printf("\n");
+//line 10
+printf("         %c%c    %c%c",219 ,219, 219,219);
+printf("  %c%c    %c%c", 219, 219, 219, 219);
+printf("      %c%c",219 ,219, 219,219);
+printf("      %c%c    %c%c", 219, 219, 219, 219);
+printf("  %c%c%c%c%c%c%c%c",219, 219, 219, 219, 219, 219, 219, 219);
+printf("\n");
+return 0;
+}
